18126bfac06d9ea59bb6d46387660f1238e19f8a
kerry <48721345+homeautomaton@users.noreply.github.com>
2024-01-16 17:40:08 -0600

    more work on command-line options

--- crosswise.py
+++ crosswise.py
@@ -1,6 +1,10 @@
 from crossview import CrossView
 from argparse import ArgumentParser, SUPPRESS
 import os
+try:
+    import readline
+except:
+    pass # will work without it
 
 # NOTES:
 #     This was developed and tested as a demonstration of how to control 
@@ -53,15 +57,19 @@ def parse_cmdline():
     p.add_argument('-l', '--location', metavar='HOME', help='Location (set up in the phone app) where to find devices')
     p.add_argument('-d', '--device', metavar='CLOCK', help='Device for data-stream operations')
     p.add_argument('-P', '--prefix', metavar='CV_', default='CV_', help='Prefix for environment variable settings')
+    p.add_argument('-i', '--id', metavar='ID', help='datastream id')
+    p.add_argument('commands', metavar='"cmd"', nargs='*', help='command(s) to run in non-interactive mode')
     return p.parse_args()
 
-def choose( prompt, what, choices, key ):
+def choose( prompt, what, choices, key, headings = None ):
     if key:
-        subset = [ c for c in choices if c[0] == key ]
+        subset = [ c for c in choices if str(c[0]) == str(key) ]
         if len( subset ) == 1:
             return what[ subset[0][1] - 1 ]
 
     print( prompt.capitalize()+ "s:" )
+    if headings:
+       for h in headings: print( h )
     for c in choices: print( c[ 2 ] )
     print( )
     count = len( what )
@@ -77,25 +85,43 @@ def choose( prompt, what, choices, key ):
             index = ''
     return what[ index - 1 ]
 
-def get_ds_id( opts ):
-    return input("Data stream identifier: ")
-
-def get_alarm_setting():
-    return input("JSON-format alarm data: ")
+def get_cmd_argument( prompt, cmds ):
+    if len( cmds ) > 0:
+        return( cmds.pop( 0 ) )
+    return input( prompt )
 
 def msg( s ):
     return '' if s == ':auto' else s
 
-def list( cv, dev ):
+def get_streams( cv, dev ):
     ds = cv.get_data_streams( dev )
     if isinstance( ds, str ):
         print( ds )
-        return
-    print( f"{'identifier'.ljust(10)} {'enable'.ljust(8)} {'sort key'.ljust(10)} {'message_one'.ljust(20)} {'message_two'.ljust(20)} {'reading'}" )
-    print( f"{'------'.ljust(10,'-')} {'-'.ljust(8,'-')} {'----'.ljust(10,'-')} {'-------'.ljust(20,'-')} {'-------'.ljust(20,'-')} {''.ljust(20,'-')}" )
-    for card in sorted( ds.get( 'cards' ), key=lambda card: -card['weight'] ) :
-        print( f"{str(card['identifier']).ljust(10)} {str(card['enabled?']).ljust(8)} {str(card['weight']).ljust(10)} " + \
-               f"{msg(card['message_one']).ljust(20)} {msg(card['message_two']).ljust(20)} {card['reading'] if 'reading' in card else ''}" )
+        return False
+    return sorted( ds.get( 'cards' ), key=lambda card: -card['weight'] )
+
+def format_streams( streams, index = False ):
+    padding = ''
+    if index: padding = '         '
+
+    headings = [ padding + f"{'identifier'.ljust(10)} {'enable'.ljust(8)} {'sort key'.ljust(10)} {'message_one'.ljust(20)} {'message_two'.ljust(20)} {'reading'}",
+                 padding + f"{'------'.ljust(10,'-')} {'-'.ljust(8,'-')} {'----'.ljust(10,'-')} {'-------'.ljust(20,'-')} {'-------'.ljust(20,'-')} {''.ljust(20,'-')}" ]
+    cards = [ [ card['identifier'], 0, f"{str(card['identifier']).ljust(10)} {str(card['enabled?']).ljust(8)} {str(card['weight']).ljust(10)} " + \
+              f"{msg(card['message_one']).ljust(20)} {msg(card['message_two']).ljust(20)} {card['reading'] if 'reading' in card else ''}" ]
+              for card in streams ]
+    for i in range( len( cards ) ): 
+        cards[i][1] = i+1
+        if index: cards[i][2] = str( i + 1 ).rjust(6) + ' - ' + cards[i][2]
+    return { "headings" : headings, "cards" : cards }
+
+def list( cv, dev ):
+    streams = get_streams( cv, dev )
+    if streams:
+        data = format_streams( streams )
+        for header in data["headings"]:
+            print(header)
+        for card in data["cards"]:
+            print(card[2])
 
 def catalog( cv ):
     n = 0
@@ -112,6 +138,14 @@ def choose_device( cv, loc, name, key ):
     choices = [ [ d.name, d.index, f"    {d.index} - {d.name} ({d.sensor_type})" ] for d in devices ]
     return choose( 'device', devices, choices, key )
 
+def choose_id( cv, loc, dev, key, type ):
+    streams = get_streams( cv, dev )
+    if streams:
+        data = format_streams( streams, index = True )
+        stream = choose( 'id', data["cards"], data["cards"], key, headings = data['headings'] )
+        return( str( stream[ 0 ] ) )
+    return None
+
 def help():
     print('Options:')
     print('    username        - enter username')
@@ -119,6 +153,7 @@ def help():
     print('    serial          - enter device serial# (from barcode on C82929 WiFi Projection Alarm Clock)')
     print('    location        - choose a location')
     print('    device          - choose a device')
+    print('    id              - choose a data stream identifier')
     print('    info            - show current values for user, serial, device and location')
     print('    show            - show alarm')
     print('    set             - set alarm')
@@ -134,6 +169,9 @@ def help():
 
 def main():
     opts = parse_cmdline()
+    have_command = len( opts.commands ) > 0
+    m1 = "message one: "
+    m2 = "message two: "
 
     dev_serial = StashedValue(lambda prompt, key: input(prompt), "Your device serial: ", opts.prefix, "device_serial")
     username = StashedValue(lambda prompt, key: input(prompt), "Username: ", opts.prefix, "user")
@@ -141,10 +179,17 @@ def main():
     cv = StashedValue(lambda prompt, key: CrossView(username.get(opts), password.get(opts)), "cv: ", opts.prefix, "cv")
     location = StashedValue(lambda prompt, key: choose_location(cv.get(), 'location', key), "Location: ", opts.prefix, "location", lookup=True)
     device = StashedValue(lambda prompt, key: choose_device(cv.get(), location.get(opts), 'device', key), "Device: ", opts.prefix, "device", lookup=True)
+    id = StashedValue(lambda prompt, key: choose_id(cv.get(), location.get(opts), device.get(opts), key, 'id'), "ID: ", opts.prefix, "id", lookup=True)
 
     miscount = 0
     while True:
-        operation = input("> ")
+        if have_command:
+            if len( opts.commands ) > 0:
+                operation = opts.commands.pop( 0 )
+            else:
+                return
+        else:
+            operation = input("> ")
         operation = operation.strip()
         missed = False
         if operation == 'help': help()
@@ -152,13 +197,14 @@ def main():
         elif operation == 'username': username.get(opts,ask=True)
         elif operation == 'password': password.get(opts,ask=True)
         elif operation == 'show': print( cv.get().get_alarm( dev_serial.get( opts ) ) )
-        elif operation == 'set': print( cv.get().set_alarm( dev_serial.get( opts ), get_alarm_setting() ) )
+        elif operation == 'id': id.get(opts,ask=True)
+        elif operation == 'set': print( cv.get().set_alarm( dev_serial.get( opts ), get_cmd_argument( "JSON-format alarm data: ", opts.commands ) ) )
         elif operation == 'catalog': catalog( cv.get() )
         elif operation == 'list': list( cv.get(), device.get(opts) )
-        elif operation == 'delete': print( cv.get().delete_data_stream( device.get(opts), get_ds_id( opts ) ) )
-        elif operation == 'get': print( cv.get().get_single_stream( device.get(opts), get_ds_id( opts ) ) )
-        elif operation == 'add': print( cv.get().add_data_stream( device.get(opts), input( "message one: " ), input( "message two: " ) ) )
-        elif operation == 'replace': print( cv.get().update_data_stream( device.get(opts), get_ds_id( opts ), input( "message one: " ), input( "message two: " ) ) )
+        elif operation == 'delete': print( cv.get().delete_data_stream( device.get(opts), id.get( opts ) ) )
+        elif operation == 'get': print( cv.get().get_single_stream( device.get(opts), id.get( opts ) ) )
+        elif operation == 'add': print( cv.get().add_data_stream( device.get(opts), get_cmd_argument( m1, opts.commands ), get_cmd_argument( m2, opts.commands ) ) )
+        elif operation == 'replace': print( cv.get().update_data_stream( device.get(opts), id.get( opts ), get_cmd_argument( m1, opts.commands ), get_cmd_argument( m2, opts.commands ) ) )
         elif operation == 'subscribe': print( cv.get().subscribe( device.get(opts), input( "subscription name or number: " ) ) )
         elif operation == 'info':
             dev_serial.show()
